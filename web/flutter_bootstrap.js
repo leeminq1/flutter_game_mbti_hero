f0cd7882bcdc10e8440744c5a98a61caedb45845
leeminq1 <min21@example.com>
2026-04-28 20:07:08 +0900

    AdSense 심사용 웹 랜딩 적용

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,11 +1,15 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.load({
-  config: {
-    hostElement: document.querySelector('#flutter-host'),
-  },
-  serviceWorkerSettings: {
-    serviceWorkerVersion: {{flutter_service_worker_version}}
-  }
-});
+const flutterHost = document.querySelector('#flutter-host');
+
+if (flutterHost) {
+  _flutter.loader.load({
+    config: {
+      hostElement: flutterHost,
+    },
+    serviceWorkerSettings: {
+      serviceWorkerVersion: {{flutter_service_worker_version}}
+    }
+  });
+}
